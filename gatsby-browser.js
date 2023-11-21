15a82683677a00769568cb69358f0221c5ae73f3
ssongJiHun <sjh9831@gmail.com>
2023-11-21 15:47:19 +0900

    'init'

--- gatsby-browser.js
+++
@@ -1,6 +0,0 @@
-// import "./src/styles/global.css"
-import './src/styles/styles.css'
-
-import "fontsource-noto-sans-kr";
-// require("prismjs/themes/prism.css");
-
--- src/markdown/about/about.md
+++
@@ -1,7 +0,0 @@
----
-template : "default"
-title: "about"
-path : '/about'
----
-# 송지훈 SONG JI HUN
-
